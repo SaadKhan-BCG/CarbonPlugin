674ceb0ea369b543c7d5c9c551bbf25009b7a533
Khan-Saad <Khan.Saad@bcg.com>
2022-11-11 15:49:54 +0000

    Query Carbon SDK for data and combine with fake power metrics

--- prometheus-exporter/exporter.py
+++ prometheus-exporter/exporter.py
@@ -1,9 +1,12 @@
 """Application exporter"""
 
 import os
+import random
 import time
 from prometheus_client import start_http_server, Gauge, Enum
 import requests
+import datetime
+from urllib.parse import urlparse, urlencode, quote
 
 
 class AppMetrics:
@@ -11,16 +14,33 @@ class AppMetrics:
     Representation of Prometheus metrics and loop to fetch and transform
     application metrics into Prometheus metrics.
     """
+    regions = ['westus', 'eastus', 'uksouth']
 
-    def __init__(self, app_port=80, polling_interval_seconds=5):
+    def __init__(self, app_host, app_port, polling_interval_seconds):
+        self.app_host = app_host
         self.app_port = app_port
         self.polling_interval_seconds = polling_interval_seconds
 
         # Prometheus metrics to collect
-        self.current_requests = Gauge("app_requests_current", "Current requests")
-        self.pending_requests = Gauge("app_requests_pending", "Pending requests")
-        self.total_uptime = Gauge("app_uptime", "Uptime")
         self.health = Enum("app_health", "Health", states=["healthy", "unhealthy"])
+        self.power_usage = Gauge("power_usage", "Power Useage (Watts)")
+        self.carbon_consumption = Gauge(name="carbon_consumption",
+                                        documentation="Carbon Consumed (gCo2Eq)",
+                                        labelnames=["region"])
+
+    # TODO refactor out into separate interface?
+    def get_current_carbon_emissions(self, location):
+        current_time = datetime.datetime.utcnow()
+        url_time = quote(current_time.strftime("%Y-%m-%dT%H:%M"))
+        prev_time = current_time - datetime.timedelta(minutes=1)
+        prev_url_time = quote(prev_time.strftime("%Y-%m-%dT%H:%M"))
+
+        # temporary
+        # request_url = f"http://{self.app_host}:{self.app_port}/emissions/bylocation?location={location}&time={prev_url_time}&toTime={url_time}"
+        request_url = f"https://{self.app_host}/emissions/bylocation?location={location}&time={prev_url_time}&toTime={url_time}"
+
+        resp = requests.get(url=request_url)
+        return resp.json()[0]['rating']
 
     def run_metrics_loop(self):
         """Metrics fetching loop"""
@@ -34,31 +54,28 @@ class AppMetrics:
         Get metrics from application and refresh Prometheus metrics with
         new values.
         """
-
-        # Fetch raw status data from the application
-        # resp = requests.get(url=f"http://localhost:{self.app_port}/status")
-        # status_data = resp.json()
-        #
-        # # Update Prometheus metrics with application metrics
-        # self.current_requests.set(status_data["current_requests"])
-        # self.pending_requests.set(status_data["pending_requests"])
-        # self.total_uptime.set(status_data["total_uptime"])
-        # self.health.state(status_data["health"])
-
-        self.current_requests.set(10)
-        self.pending_requests.set(42)
-        self.total_uptime.set(230)
         self.health.state("healthy")
+
+        current_power_consumption = random.randint(10, 100)
+
+        self.power_usage.set(current_power_consumption)
+        for region in self.regions:
+            carbon = self.get_current_carbon_emissions(region)
+            self.carbon_consumption.labels(region).set(current_power_consumption * carbon)
 
 
 def main():
     """Main entry point"""
 
     polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "5"))
-    app_port = int(os.getenv("APP_PORT", "80"))
+    app_port = int(os.getenv("CARBON_SDK_PORT", "80"))
+    app_host = int(os.getenv("CARBON_SDK_HOST", "80"))
     exporter_port = int(os.getenv("EXPORTER_PORT", "9877"))
 
+    app_host = "carbon-aware-api.azurewebsites.net"     # temporary
+
     app_metrics = AppMetrics(
+        app_host=app_host,
         app_port=app_port,
         polling_interval_seconds=polling_interval_seconds
     )
