7035709c66f6edf04fe403015bc6cad3e0b6ec34
Khan-Saad <Khan.Saad@bcg.com>
2022-11-17 15:44:58 +0000

    Fixed carbon calculations

--- prometheus-exporter/container_stats.py
+++ prometheus-exporter/container_stats.py
@@ -3,7 +3,6 @@ from constants import energy_profile
 
 
 def get_container_stats(docker_client, container):
-
     stats = docker_client.containers.get(container).stats(stream=False)
 
     # CPU Power
@@ -11,11 +10,11 @@ def get_container_stats(docker_client, container):
     system_delta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
     len_cpu = stats['cpu_stats']['online_cpus']
     percentage = (usage_delta / system_delta) * len_cpu * 100
-    cpu_power = percentage * len_cpu * energy_profile['PUE'] * energy_profile['CPU'] / 3600
+    cpu_power = percentage * energy_profile['PUE'] * energy_profile['CPU'] / 3600
 
     # Memory Power
-    mem_usage = stats['memory_stats']['usage'] / 1073741824   # Number is in bytes so divide to get to GB
-    mem_power = mem_usage * energy_profile['PUE'] * energy_profile['MEM'] / 60
+    mem_usage = stats['memory_stats']['usage'] / 1073741824  # Number is in bytes so divide to get to GB
+    mem_power = mem_usage * energy_profile['PUE'] * energy_profile['MEM'] / 3600
 
     # Network Power
     total_rx = 0
@@ -24,10 +23,12 @@ def get_container_stats(docker_client, container):
         total_rx += network['rx_bytes']
         total_tx += network['tx_bytes']
 
-    network_power = (total_tx + total_rx)/1073741824 * energy_profile['NETWORK']/60
+    network_power = (total_tx + total_rx) / 1073741824 * energy_profile['NETWORK'] / 7200
     disk_power = 0  # Usually almost nothing
 
-    return cpu_power + mem_power + disk_power + network_power
+    # Note total data is in watts/s (CarbonAware SDK works in gCo2/KwH)
+    total_power = (cpu_power + mem_power + disk_power + network_power)
+    return total_power
 
 
 def get_stats():
@@ -37,13 +38,3 @@ def get_stats():
         if not containers.name.startswith("carbon-plugin"):
             stats[containers.name] = get_container_stats(client, containers.id)
     return stats
-
-
-# def run_metrics_loop():
-#     """Metrics fetching loop"""
-#
-#     while True:
-#         print(get_stats())
-#
-#
-# run_metrics_loop()
