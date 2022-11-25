396e6f546d26d38b5ec40e9d83fd800770735340
Khan-Saad <Khan.Saad@bcg.com>
2022-11-25 16:09:40 +0000

    Comments

--- carbon-monitor/main.go
+++ carbon-monitor/main.go
@@ -14,6 +14,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// TODO put this somewhere better, ideally make it configurable via cli ie allow a --all option/ commar separated list of ones you care about
 var regions = []string{
 	"australiacentral",
 	"australiacentral2",
